6e7f31ef512880a015bc559f2d9b6f2f5e8003e4
tdreid <trevor.d.reid@gmail.com>
2018-05-05 19:38:20 +0000

    Add save, load and clear methods to viewmodel

--- public/js/app.js
+++ public/js/app.js
@@ -33,6 +33,36 @@ window.onload = function() {
         );
       }
     }.bind(this);
+
+    this.saveViewModel = function() {
+      var flattenedTests = self.tests().map(t => {
+        return ko.toJSON(t);
+      });
+      localforage.setItem('10mwt', flattenedTests);
+    };
+
+    this.loadViewModel = function() {
+      localforage.getItem('10mwt').then(savedtests => {
+        savedtests.forEach(item => {
+          item = JSON.parse(item);
+          let savedtrials = item.trials.map(savedTrial => {
+            let newTrial = new Trial();
+
+            newTrial.s(savedTrial.s);
+            newTrial.s2(savedTrial.s2);
+            newTrial.state(savedTrial.state);
+            newTrial.stateLabel(savedTrial.stateLabel);
+
+            return newTrial;
+          });
+          self.tests.push(new Test(item.id, savedtrials));
+        });
+      });
+    };
+
+    this.clearViewModel = function() {
+      self.tests([]);
+    };
   };
 
   var Test = function(id = 'New Test', trials = []) {
